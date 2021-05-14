42b2b3c3a256459491a026f323a70e5a1940632b
seeker <populpock2@gmail.com>
2021-05-14 05:16:49 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,67 +1,107 @@
 
+// Написать программу, выводящую на экран все двоичные вектора длины 32, имеющие заданный вес n <= 4.
+
 #define _CRT_SECURE_NO_WARNINGS
-// Написать программу, выводящую на экран все двоичные вектора длины 32,
-// имеющие заданный вес n <= 4.
 
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
-// Function to generate all binary strings
-void generateAllBinaryStrings(int* length, int* arr, int i, int* n)
-{
-	if (i == *length) {
-		int tmp = 0;
-		for (int q = 0; q < *length; q++)
-			tmp += arr[q];
-
-		if (tmp == *n)
-		{
-			printf("\n\t");
-			for (int j = 0; j < *length; j++) {
-				printf(" %d ", arr[j]);
-			}
-		}
-		return;
-	}
-
-	// убрать переборный алгоритм
-	// без проверок
-
-	// First assign "0" at ith position
-	// and try for all other permutations
-	// for remaining positions
-	arr[i] = 0;
-	generateAllBinaryStrings(length, arr, i + 1, n);
-	// And then assign "1" at ith position
-	// and try for all other permutations
-	// for remaining positions
-	arr[i] = 1;
-	generateAllBinaryStrings(length, arr, i + 1, n);
+void swap(int* l, int* r) {
+    if (*l != *r) {
+        *l ^= *r;
+        *r ^= *l;
+        *l ^= *r;
+    }
 }
 
-int main()
-{
-	int length, n, i = 0;
+void reverse(int* begin, int* end) {
+    if (begin < end - 1) {
+        swap(begin, end - 1);
+        reverse(++begin, --end);
+    }
+}
 
-	printf("\n Input vecor length: ");
-	if (scanf("%d", &length) != 1)
-	{
-		printf("\n Input Error");
-		return -1;
-	}
+int gen_permutation(int* begin, int* end) {
+    int* next = end;
 
-	printf(" Input max vector mass: ");
-	if (scanf("%d", &n) != 1)
-	{
-		printf("\n Input Error");
-		return -1;
-	}
+    if (begin == end || begin == --next) {
+        return 0;
+    }
 
-	int* arr = (int*)malloc(length * sizeof(int));
+    while (1) {
+        int* next1 = next;
+        if (*--next < *next1) {
+            int* mid = end;
+            do {
+                --mid;
+            } while (*next >= *mid);
 
-	// Print all binary strings
-	printf("\n Result: ");
-	generateAllBinaryStrings(&length, arr, i, &n);
-	printf("\n");
-	return 0;
+            swap(next, mid);
+            reverse(next1, end);
+            return 1;
+        }
+
+        if (next == begin) {
+            reverse(begin, end);
+            return 0;
+        }
+    }
+}
+
+int main() {
+    int counter = 1;
+
+    int n = 0;
+    printf("\n Input vector length: ");
+    if (((scanf("%d", &n)) != 1) || n <= 0)
+    {
+        printf("\n Input error");
+        return -2;
+    }
+
+    int max = 0;
+    printf(" Input max vector mass: ");
+    if (((scanf("%d", &max)) != 1) || max > n)
+    {
+        printf("\n Input error");
+        return -2;
+    }
+
+    int* vector = (int*)calloc(n, sizeof(int) * n);
+    if (!vector)
+    {
+        printf("\t Memory allocation error");
+        return -2;
+    }
+    
+    printf("\n Result:");
+
+    printf("\n  %d.          ", counter);
+    for (int j = 0; j < n; j++)
+        printf("%d ", vector[j]);
+
+    if (n != max)
+    {
+        for (int i = 0; i < max; i++)
+        {
+            vector[n - 1 - i] = 1;
+            do {
+                counter++;
+                printf("\n  %d.          ", counter);
+                for (int j = 0; j < n; j++)
+                    printf("%d ", vector[j]);
+            } while (gen_permutation(vector, vector + n));
+        }
+    }
+    else
+    {
+        printf("\n\t");
+        for (int j = 0; j < n; j++)
+        {
+            vector[j] = 1;
+            printf("%d ", vector[j]);
+        }
+    }
+
+    return 0;
 }
